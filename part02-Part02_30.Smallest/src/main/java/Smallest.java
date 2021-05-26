d19fdc277098af7648cfb5d6b978b1934f840d83
Pete Kriengsiri <pkriengsiri@gmail.com>
2021-05-26 12:34:22 -0400

    finished part 2

--- part02-Part02_30.Smallest/src/main/java/Smallest.java
+++ part02-Part02_30.Smallest/src/main/java/Smallest.java
@@ -1,11 +1,12 @@
 
 public class Smallest {
 
+//    Define a two-parameter method smallest that returns the smaller of the two numbers passed to it as parameters.
     public static int smallest(int number1, int number2) {
         // Write some code here
         // don't print anything inside this method
         // there must be a return in the end of the method
-        return -1;
+        return Math.min(number1, number2);
     }
 
     public static void main(String[] args) {
